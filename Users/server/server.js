14904c48bc1b79b055655836ec6d216409749675
Galochkin Daniil <galo4kin123danilo@gmail.com.com>
2022-10-17 18:55:12 +0300

    fixed search method

--- Users/server/server.js
+++ Users/server/server.js
@@ -4,28 +4,28 @@ const fastify = require('fastify')({ logger: true });
 fastify.register(require('fastify-cors'), {});
 
 fastify.get('/', async (request, reply) => {
-	fs.readFile('./users.json', 'utf8', (err, data) => {
-		if (err) {
-			console.log('File read failed:', err);
-			return;
-		}
+  fs.readFile('./users.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log('File read failed:', err);
+      return;
+    }
 
-		if(request.query.term)
-		{
-			const result = JSON.parse(data).filter((elem)=> elem.name.toLowerCase().search(request.query.term.toLowerCase()) !== -1);
-			reply.send(JSON.stringify(result));
-		}
-		else
-		{
-			reply.send(data);
-		}
-
-	})
+    if (request.query.term) {
+      const result = JSON.parse(data).filter(
+        (elem) =>
+          elem.name.toLowerCase().search(request.query.term.toLowerCase()) !==
+          -1
+      );
+      reply.send(JSON.stringify(result));
+    } else {
+      reply.send(data);
+    }
+  });
 });
 
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(3000);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
